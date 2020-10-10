ffafc937a040817e94556422bcc1d37c01ede549
Kevin <kevinrenfc@gmail.com>
2020-10-10 16:11:53 -0700

    Full SkystoneAuto simulation

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/SkystoneAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/SkystoneAuto.java
@@ -18,20 +18,45 @@ public class SkystoneAuto extends LinearOpMode {
 
         drive.setPoseEstimate(startPose);
 
-        Trajectory collection = drive.trajectoryBuilder(startPose)
+        Trajectory collection1 = drive.trajectoryBuilder(startPose)
                 .forward(24)
                 .splineTo(new Vector2d(-43, 24), Math.toRadians(235))
                 .build();
 
-        Trajectory deposit = drive.trajectoryBuilder(collection.end(), true)
+        Trajectory deposit1 = drive.trajectoryBuilder(collection1.end(), true)
                 .splineTo(new Vector2d(48, 32), Math.toRadians(270))
+                .build();
+
+        Trajectory collection2 = drive.trajectoryBuilder(deposit1.end())
+                .splineTo(new Vector2d(-34.0, 24.0), Math.toRadians(235))
+                .build();
+
+        Trajectory deposit2 = drive.trajectoryBuilder(collection2.end(), true)
+                .splineTo(new Vector2d(48, 32), Math.toRadians(270))
+                .build();
+
+        Trajectory collection3 = drive.trajectoryBuilder(deposit2.end())
+                .splineTo(new Vector2d(-25.0, 24.0), Math.toRadians(235))
+                .build();
+
+        Trajectory deposit3 = drive.trajectoryBuilder(collection3.end(), true)
+                .splineTo(new Vector2d(48, 32), Math.toRadians(270))
+                .build();
+
+        Trajectory park = drive.trajectoryBuilder(deposit3.end())
+                .strafeLeft(48)
                 .build();
 
         waitForStart();
 
         if(isStopRequested()) return;
 
-        drive.followTrajectory(collection);
-        drive.followTrajectory(deposit);
+        drive.followTrajectory(collection1);
+        drive.followTrajectory(deposit1);
+        drive.followTrajectory(collection2);
+        drive.followTrajectory(deposit2);
+        drive.followTrajectory(collection3);
+        drive.followTrajectory(deposit3);
+        drive.followTrajectory(park);
     }
 }
