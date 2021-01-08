4703f543e6fe9ac4c19d52fdfa55873520fff4f0
Kevin <kevinrenfc@gmail.com>
2021-01-07 16:50:58 -0800

    added intake & shooter teleOp code

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/Intake.java
@@ -0,0 +1,56 @@
+package org.firstinspires.ftc.teamcode.drive;
+
+import com.qualcomm.robotcore.hardware.Gamepad;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.Servo;
+
+public class Intake {
+    private Servo servo;
+    private CRServo crServo;
+
+    private double dropPosition = 0.5;
+    private double liftPosition = 0.7;
+
+    public Intake(Servo servo, CRServo crServo) {
+        this.servo = servo;
+        this.crServo = crServo;
+
+    }
+
+    public void controls(Gamepad gp) {
+        if(gp.dpad_down) {
+            dropIntake();
+        }
+        else if(gp.dpad_up) {
+            liftIntake();
+        }
+
+        if(gp.dpad_left) {
+            reverseRing();
+        }
+        else if(gp.dpad_right) {
+            pushRing();
+        }
+        else stopRing();
+    }
+
+    public void dropIntake() {
+        servo.setPosition(dropPosition);
+    }
+
+    public void liftIntake() {
+        servo.setPosition(liftPosition);
+    }
+
+    public void pushRing() {
+        crServo.setPower(0.5);
+    }
+
+    public void reverseRing() {
+        crServo.setPower(-0.5);
+    }
+
+    public void stopRing() {
+        crServo.setPower(0.0);
+    }
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/CorrectTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/CorrectTeleOp.java
@@ -6,16 +6,22 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.teamcode.drive.Drivetrain;
+import org.firstinspires.ftc.teamcode.drive.Intake;
+import org.firstinspires.ftc.teamcode.drive.Shooter;
 
 
 @TeleOp(name = "TeleOp", group = "jerW" )
 public class CorrectTeleOp extends OpMode {
     //TODO: Create instance variable for drivetrain
     public Drivetrain drivetrain;
+    public Intake intake;
+    public Shooter shooter;
     @Override
     public void init() {
         //TODO: Initialize Drivetrain Object
         drivetrain = new Drivetrain(hardwareMap.dcMotor.get("frontLeftMotor"), hardwareMap.dcMotor.get("backLeftMotor"), hardwareMap.dcMotor.get("frontRightMotor"), hardwareMap.dcMotor.get("backRightMotor"), false, telemetry, hardwareMap);
+        intake = new Intake(hardwareMap.servo.get("useless"), hardwareMap.crservo.get("legsOfDoom"));
+        shooter = new Shooter(hardwareMap.dcMotor.get("shooter"));
     }
 
     @Override
@@ -28,6 +34,8 @@ public class CorrectTeleOp extends OpMode {
     public void loop() {
         //TODO: Controls
         drivetrain.controls(gamepad1);
+        intake.controls(gamepad1);
+        shooter.controls(gamepad1);
     }
 
     @Override
