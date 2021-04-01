26836a944a5056281b167a6199009b055523dfb3
Unknown <maxyeh@gmail.com>
2021-04-01 16:47:15 -0700

    Changed some constants
    Made vision work (pre init detection). Need to add to main auto code thingy

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/UltimateGoalPipeline.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/UltimateGoalPipeline.java
@@ -16,13 +16,13 @@ public class UltimateGoalPipeline extends OpenCvPipeline {
     // color constants and other constants
     private static final Scalar BLUE = new Scalar(0,0,255);
     private static final Scalar GREEN = new Scalar(0,255,0);
-    private static final int REGION_WIDTH = 75;
+    private static final int REGION_WIDTH = 70;
     private static final int REGION_HEIGHT = 55;
-    private final int FOUR_RING_THRESHOLD = 145;
-    private final int ONE_RING_THRESHOLD = 135;
+    private final int FOUR_RING_THRESHOLD = 135; // This can really be changed (based on the time of day)
+    private final int ONE_RING_THRESHOLD = 120; // As long as the ONE_RING_THRESHOLD <= FOUR_RING_THRESHOLD - 10
 
     // camera positioning location
-    private static final Point ANCHOR_POINT = new Point(115, 105); // anchor point for box to be drawn
+    private static final Point ANCHOR_POINT = new Point(95, 105); // anchor point for box to be drawn
     private Point REGION_TOP_LEFT = new Point(ANCHOR_POINT.x, ANCHOR_POINT.y);
     private Point REGION_BOTTOM_RIGHT = new Point(ANCHOR_POINT.x + REGION_WIDTH, ANCHOR_POINT.y + REGION_HEIGHT);
 
@@ -58,7 +58,7 @@ public class UltimateGoalPipeline extends OpenCvPipeline {
         else
             pos = Rings.ZERO;
 
-        Imgproc.rectangle(in, REGION_TOP_LEFT, REGION_BOTTOM_RIGHT, GREEN, -1);
+        // Imgproc.rectangle(in, REGION_TOP_LEFT, REGION_BOTTOM_RIGHT, GREEN, -1);
 
         return in;
     }
