9754e13488680090d1846d633af921f692c7ee58
Unknown <maxyeh@gmail.com>
2021-04-22 17:00:42 -0700

    Pos Estimation
    Used openCV to detect discs

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/VisionTest2.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/VisionTest2.java
@@ -4,6 +4,7 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.teamcode.drive.PosDetection;
 import org.firstinspires.ftc.teamcode.drive.UltimateGoalPipeline;
 import org.openftc.easyopencv.OpenCvCamera;
 import org.openftc.easyopencv.OpenCvCameraFactory;
@@ -13,14 +14,14 @@ import org.openftc.easyopencv.OpenCvCameraRotation;
 public class VisionTest2 extends LinearOpMode {
 
     OpenCvCamera camera;
-    UltimateGoalPipeline pipeline;
+    PosDetection pipeline;
 
     @Override
     public void runOpMode() throws InterruptedException {
 
         int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
         camera = OpenCvCameraFactory.getInstance().createWebcam(hardwareMap.get(WebcamName.class, "camera"), cameraMonitorViewId);
-        pipeline = new UltimateGoalPipeline();
+        pipeline = new PosDetection();
         camera.setPipeline(pipeline);
 
         camera.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener() {
@@ -33,8 +34,7 @@ public class VisionTest2 extends LinearOpMode {
         waitForStart();
         while(opModeIsActive()) {
 
-            telemetry.addData("Analysis", pipeline.getOutput());
-            telemetry.addData("Position", pipeline.pos);
+            telemetry.addData("Analysis", pipeline.getContours());
             telemetry.update();
 
             // Don't burn CPU cycles busy-looping in this sample
